c0f7260406e4b7afcf6810052ea6815923c2249f
Setiawan Joko <setyawanj65@gmail.com>
2024-09-18 16:23:05 +0700

    Add | Body Parser

--- index.js
+++ index.js
@@ -1,9 +1,17 @@
 const express = require('express')
+const bodyParser  = require('body-parser')
 const app = express()
 const port = 3000
 
+app.use(bodyParser.json())
+
 app.get('/', (req, res) => {
   res.send('Hello World!')
+})
+
+app.post('/', (req, res) => {
+    console.log({requestFromOutside: req.body})
+    res.send('Login berhasil')
 })
 
 app.listen(port, () => {
