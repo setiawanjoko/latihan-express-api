f0e60a041b76095f6e3a6787e569f7a342209b0e
Setiawan Joko <setyawanj65@gmail.com>
2024-09-19 10:18:15 +0700

    Add | Salgrade endpoint GET method

--- index.js
+++ index.js
@@ -1,30 +1,50 @@
-import express from 'express'
-import bodyParser from 'body-parser'
-import client from './src/services/Connection.js'
-import cors from 'cors'
-import config from './src/commons/config.js'
+import express from "express";
+import bodyParser from "body-parser";
+import client from "./src/services/Connection.js";
+import cors from "cors";
+import config from "./src/commons/config.js";
 
-const app = express()
+const app = express();
 
-app.use(cors({
-  origin: '*'
-}))
-app.use(bodyParser.json())
+app.use(
+  cors({
+    origin: "*",
+  })
+);
+app.use(bodyParser.json());
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
 
-app.post('/', (req, res) => {
-    console.log({requestFromOutside: req.body})
-    res.send('Login berhasil')
-})
+app.post("/", (req, res) => {
+  console.log({ requestFromOutside: req.body });
+  res.send("Login berhasil");
+});
+
+app.get("/salgrade", (req, res) => {
+  client.query("SELECT * FROM SALGRADE", (err, result) => {
+    if (!err) res.send(result.rows);
+    else res.send(err.message);
+  });
+});
+
+app.get("/salgrade/:grade", (req, res) => {
+    let { grade } = req.params
+  client.query(
+    `SELECT * FROM SALGRADE WHERE grade = ${grade}`,
+    (err, result) => {
+      if (!err) res.send(result.rows[0]);
+      else res.send(err.message);
+    }
+  );
+});
 
 app.listen(config.app.port, () => {
-  console.log(`Example app listening on port ${config.app.port}`)
-})
+  console.log(`Example app listening on port ${config.app.port}`);
+});
 
-client.connect(err => {
-  if(err) console.log(err.message)
-    else console.log('Database connected')
+client.connect((err) => {
+  if (err) console.log(err.message);
+  else console.log("Database connected");
 });
